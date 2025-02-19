8bc133e4b8e9a57061399db8b71e65c92927d23a
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2025-02-19 13:38:51 +0100

    update build

--- 9504.4987865c6b0e7f61.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunklpda2=self.webpackChunklpda2||[]).push([[9504],{9504:(l,s,n)=>{n.r(s),n.d(s,{routes:()=>t});const t=[{path:"",loadComponent:()=>Promise.all([n.e(1560),n.e(2448),n.e(1868),n.e(9312),n.e(7592)]).then(n.bind(n,5948)).then(o=>o.ListPreferencesComponent)}]}}]);
